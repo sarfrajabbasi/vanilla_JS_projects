29fd9e0d757436792b1b00aed4ae3e72783b1586
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-24 13:57:01 +0530

    vanilla js projects

--- questions/setup/app.js
+++ questions/setup/app.js
@@ -1,2 +1,14 @@
 //using selectors inside the element
+
+
 // traversing the dom
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        const question = e.currentTarget.parentElement.parentElement;
+
+        question.classList.toggle('show-text');
+        
+    })
+})
