518af61b9e65e5a598c00c8f6d9d969126a253a3
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-24 14:06:32 +0530

    vanilla js projects

--- questions/setup/app.js
+++ questions/setup/app.js
@@ -1,14 +1,29 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question)=>{
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click',function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text')
+    })
+})
+
 
 
 // traversing the dom
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
+// console.log(btns);
+// btns.forEach((btn)=>{
+//     btn.addEventListener('click',function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         console.log(question);
 
-btns.forEach((btn)=>{
-    btn.addEventListener('click',(e)=>{
-        const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
 
-        question.classList.toggle('show-text');
-        
-    })
-})
+//     })
+// })
