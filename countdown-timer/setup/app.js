0dda804ca5064e7e83c99477b811ecb6f54f40b6
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-25 12:39:32 +0530

    vanilla js projects

--- countdown-timer/setup/app.js
+++ countdown-timer/setup/app.js
@@ -27,19 +27,26 @@ const deadline = document.querySelector(".deadline");
 
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureData = new Date(2023, 11, 31, 12, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
-const year = futureData.getFullYear();
-const hours = futureData.getHours();
-const mins = futureData.getMinutes();
-const secs = futureData.getSeconds();
-const month = futureData.getMonth();
-const date = futureData.getDate();
-const day = futureData.getDay();
+// let futureDate = new Date(2023, 11, 31, 12, 30, 0);
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+// console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const secs = futureDate.getSeconds();
+const month = futureDate.getMonth();
+const date = futureDate.getDate();
+const day = futureDate.getDay();
 
 giveaway.textContent = `giveaway ends on ${weekdays[day]}, ${date}  ${months[month]} ${year}, ${hours}:${mins}:${secs} am`;
 
-const futurTime = futureData.getTime();
+const futurTime = futureDate.getTime();
 
 function getRemainingTime() {
   const today = new Date().getTime();
@@ -71,10 +78,11 @@ function getRemainingTime() {
     item.innerHTML = format(dateValues[index]);
   });
   if (remainingTime < 0) {
-    clearInterval(coundDown)
-    deadline.innerHTML = '<h4 class=expired>Sorry, this giveaway has expired</h4>'
+    clearInterval(coundDown);
+    deadline.innerHTML =
+      "<h4 class=expired>Sorry, this giveaway has expired</h4>";
   }
 }
 // count down
 let coundDown = setInterval(getRemainingTime, 1000);
-getRemainingTime()
+getRemainingTime();
