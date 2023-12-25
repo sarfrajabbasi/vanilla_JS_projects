c01691496c63ee540ced4070e385faf8803132d0
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-25 11:49:38 +0530

    vanilla js projects

--- countdown-timer/setup/app.js
+++ countdown-timer/setup/app.js
@@ -21,3 +21,23 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureData = new Date(2024,1,10,30,0);
+
+const year = futureData.getFullYear();
+const hours = futureData.getHours();
+const mins = futureData.getMinutes();
+const secs = futureData.getSeconds();
+const month = futureData.getMonth();
+const date = futureData.getDate()
+const day = futureData.getDay();
+
+giveaway.textContent = `giveaway ends on ${weekdays[day]}, ${date}  ${months[month]} ${year}, ${hours}:${mins}:${secs} am`
+
+const futurTime = futureData.getTime();
+console.log(futurTime);
