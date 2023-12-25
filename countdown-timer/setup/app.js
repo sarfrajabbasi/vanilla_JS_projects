8131cc4a8741363dd3973fcedc17f2c523a62ae5
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-25 12:31:36 +0530

    vanilla js projects

--- countdown-timer/setup/app.js
+++ countdown-timer/setup/app.js
@@ -22,22 +22,59 @@ const weekdays = [
   "Saturday",
 ];
 
-const giveaway = document.querySelector('.giveaway');
-const deadline = document.querySelector('.deadline');
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
 
-const items = document.querySelectorAll('.deadline-format h4');
+const items = document.querySelectorAll(".deadline-format h4");
 
-let futureData = new Date(2024,1,10,30,0);
+let futureData = new Date(2023, 11, 31, 12, 30, 0);
 
 const year = futureData.getFullYear();
 const hours = futureData.getHours();
 const mins = futureData.getMinutes();
 const secs = futureData.getSeconds();
 const month = futureData.getMonth();
-const date = futureData.getDate()
+const date = futureData.getDate();
 const day = futureData.getDay();
 
-giveaway.textContent = `giveaway ends on ${weekdays[day]}, ${date}  ${months[month]} ${year}, ${hours}:${mins}:${secs} am`
+giveaway.textContent = `giveaway ends on ${weekdays[day]}, ${date}  ${months[month]} ${year}, ${hours}:${mins}:${secs} am`;
 
 const futurTime = futureData.getTime();
-console.log(futurTime);
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const remainingTime = futurTime - today;
+  // 1s =1000ms
+  // 1m =60s
+  // 1hr =60min
+  // 1d =24hr
+  // values in ms
+  const oneday = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMin = 60 * 1000;
+  // calculate all values
+  let days = Math.floor(remainingTime / oneday);
+  let hours = Math.floor((remainingTime % oneday) / oneHour);
+  let minutes = Math.floor((remainingTime % oneHour) / oneMin);
+  let seconds = Math.floor((remainingTime % oneMin) / 1000);
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    } else {
+      return item;
+    }
+  }
+  // set value arrays
+  const dateValues = [days, hours, minutes, seconds];
+  items.forEach((item, index) => {
+    item.innerHTML = format(dateValues[index]);
+  });
+  if (remainingTime < 0) {
+    clearInterval(coundDown)
+    deadline.innerHTML = '<h4 class=expired>Sorry, this giveaway has expired</h4>'
+  }
+}
+// count down
+let coundDown = setInterval(getRemainingTime, 1000);
+getRemainingTime()
