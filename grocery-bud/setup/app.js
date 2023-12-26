42c72b24eb123321e182090e7708e503fafea797
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-26 18:19:00 +0530

    vanilla js projects

--- grocery-bud/setup/app.js
+++ grocery-bud/setup/app.js
@@ -13,38 +13,67 @@ let editFlag = false;
 let editID = "";
 
 // ****** EVENT LISTENERS **********
-form.addEventListener("submit",addItem);
-
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
-    e.preventDefault()
+  e.preventDefault();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if (value && !editFlag ) {
-    console.log("add item to the list");
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` 
+    <p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+        </button>
+        <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+        </button>
+        </div>`;
+    // append child
+    list.appendChild(element);
+
+    // display alert
+    displayAlert("add item to the list", "success");
+    // show conatiner
+    container.classList.add("show-container");
+    // add to local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
   } else if (value && editFlag) {
     console.log("editing");
   } else {
-    displayAlert('please enter value',"danger")
-    
+    displayAlert("please enter value", "danger");
   }
 }
 
-function displayAlert(text,action){
-    alertMessage.textContent = text;
-    alertMessage.classList.add(`alert-${action}`)
+function displayAlert(text, action) {
+  alertMessage.textContent = text;
+  alertMessage.classList.add(`alert-${action}`);
 
-    // remove alert
-    setTimeout(function(){
-        alertMessage.textContent = '';
-        alertMessage.classList.remove(`alert-${action}`)
-    
-    },1000)
-
+  // remove alert
+  setTimeout(function () {
+    alertMessage.textContent = "";
+    alertMessage.classList.remove(`alert-${action}`);
+  }, 1000);
 }
 
+// set back to default
+function setBackToDefault() {
+  console.log("set back to deafult");
+}
 
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log("add to local storage");
+}
 // ****** SETUP ITEMS **********
